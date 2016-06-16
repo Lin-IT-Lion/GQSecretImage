5d96d4c4360a4242972f9ea9fa4a46581f3bafff
LinGuoQiang <lin_it@outlook.com>
2016-06-16 09:58:13 +0800

    ［del］
    删除pod文件

--- GQSecretImageDemo/Pods/Headers/Private/GQSecretImage/GQSecretImageModel.h
+++
@@ -1,1 +0,0 @@
-../../../../../GQSecretImage/GQSecretImageModel.h
--- GQSecretImageDemo/Pods/Target Support Files/GQSecretImage/GQSecretImage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GQSecretImage : NSObject
-@end
-@implementation PodsDummy_GQSecretImage
-@end
